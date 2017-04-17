0ef4f0f633226346505e50d0b44f50852b31b142
657994434 <657994434@qq.com>
2017-04-17 11:37:58 +0800

    int转时间

---
+++ src/main/exam/DateDemo.java
@@ -0,0 +1,15 @@
+package main.exam;
+
+import java.util.Date;
+
+/**
+ * @author 李智
+ * @date 2017/4/17
+ */
+public class DateDemo {
+    public static void main(String[] args) {
+        int i = 100000;
+        Date date = new Date(i);
+        System.out.println(date);
+    }
+}
