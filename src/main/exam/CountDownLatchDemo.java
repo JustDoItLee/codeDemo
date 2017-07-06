2d4891b5acc97cd46bd6a4ba5ef253faf1e7fc9a
657994434 <657994434@qq.com>
2017-07-06 17:37:04 +0800

    倒计时

---
+++ src/main/exam/CountDownLatchDemo.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author 李智
+ * @date 2017/7/6
+ */
+public class CountDownLatchDemo implements Runnable {
+    static final CountDownLatch end = new CountDownLatch(10);
+    static final CountDownLatchDemo demo = new CountDownLatchDemo();
+
+    public void run() {
+        try {
+            Thread.sleep(new Random().nextInt(10) * 1000);
+            System.out.println("准备完毕");
+            end.countDown();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService exec = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10; i++) {
+            exec.submit(demo);
+        }
+        end.await();
+        System.out.println("发射");
+        exec.shutdown();
+    }
+}
